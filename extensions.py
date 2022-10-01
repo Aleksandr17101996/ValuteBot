826702b7495199d3d30e443bbd03ba8f92bc70f2
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2022-10-01 18:54:55 +0300

    Version 1.0

---
+++ extensions.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+
+class APIExtension(Exception):
+    def __init__(self, text):
+        self.txt = text
+
+
+class CryptoComp:
+    @staticmethod
+    def get_prices(base, quote, amount):
+        crypto_data = requests.get(
+            "https://min-api.cryptocompare.com/data/pricemultifull?fsyms={}&tsyms={}".format(base,quote)).json()['RAW']
+        crypto_data = json.dumps(crypto_data)
+        load_data = json.loads(crypto_data)
+        price = load_data[base][quote]["PRICE"] * amount
+        #data = {}
+        #for i in load_data:
+        #    data[i] = {
+        #        "price": load_data[i][quote]["PRICE"],
+        #    }
+        return round(price)
+
+
+if __name__ == "__main__":
+    a = CryptoComp()
+    print(a.get_prices('EUR', 'RUB', 100))
---
+++ tkn_config.py
@@ -0,0 +1,2 @@
+token = "5732351518:AAFaezZLYrv10BTNTZZIJ-CCvouxl41ft2w"
+list_valute = ['USD', 'EUR', 'RUB']
